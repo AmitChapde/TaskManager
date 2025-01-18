4e4f0c095d77a457673079d5427097c54b86b2b8
Amit Chapde <amitchapde2@gmail.com>
2025-01-18 15:18:26 +0530

    added backend changes for URI

--- Backend/index.js
+++ Backend/index.js
@@ -2,38 +2,31 @@ const cors = require("cors");
 require('dotenv').config();
 const express = require("express");
 const mongoose = require("mongoose");
-const taskModel = require("./models/task.model");
-const TaskService = require("./services/task.service");
-const TaskServiceInstance = new TaskService();
-const {
-    getTasks,
-    createTask,
-    updateTask,
-    deleteTask,
-  } = require("./controllers/task.controller");
 const taskRoutes = require("./routes/task.routes");
-  
-
 
 const app = express();
 const PORT = 8082;
-const DB_URI = process.env.MONGO_URI;
-app.use(cors());
-app.use(express.json()); 
 
+const DB_URI = `mongodb+srv://AmitChapde:${encodeURIComponent(process.env.DB_PASSWORD)}@taskmanager.499kz.mongodb.net/taskmanager?retryWrites=true&w=majority&appName=taskManager`;
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Routes
 app.use("/tasks", taskRoutes);
 
+// MongoDB Connection
 mongoose
   .connect(DB_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
+    family: 4, 
   })
   .then(() => console.log("DB Connected!"))
-  .catch((error) => console.log("Error in connecting DB", error));
+  .catch((error) => console.error("Error in connecting DB", error));
 
-app.use(cors());
-app.use(express.json());
-
+// Start Server
 app.listen(PORT, () => {
   console.log(`Backend listening on Port ${PORT}!`);
 });
